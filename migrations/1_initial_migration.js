afdc72f46b8359057d40a9be319d458c490183d5
lamine <45393240+lamine17@users.noreply.github.com>
2020-12-07 00:08:10 +0100

    Add files via upload

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require('Migrations')
+const Migrations = artifacts.require("Migrations");
+const Wikipedia = artifacts.require("Wikipedia");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations)
-}
+  deployer.deploy(Migrations);
+  deployer.deploy(Wikipedia);
+};
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-const Wikipedia = artifacts.require('Wikipedia')
+var Migrations=artifacts.require ("./Migrations.sol");
+var Wikipedia=artifacts.require ("./Wikipedia.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(Wikipedia)
+module.exports = function(deployer) {      
+	deployer.deploy(Wikipedia);
+	deployer.deploy(Migrations);
 }
